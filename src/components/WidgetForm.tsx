6a3536e3c73797c211116123042ed4ac2715b2b7
Gesiel Neves <neves.gesiel@gmail.com>
2022-05-08 00:42:24 -0300

    Criado feedbackTypes e assets

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -1,5 +1,18 @@
 import { CloseButton } from "./CloseButton";
 
+const feedbackTypes = {
+  BUG: {
+    title: 'Problema'
+  },
+  IDEA: {
+    title: 'Ideia'
+  },
+  OTHER: {
+    title: 'Outro'
+  },
+
+};
+
 export function WidgetForm() {
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
@@ -8,7 +21,11 @@ export function WidgetForm() {
 
                 <CloseButton />
             </header>
-            <p> Hello World</p>
+
+            <div className="flex py-8 gap-2 w-full">
+
+
+            </div>
 
             <footer>
             Feito com ♥ pela  <a className="underline underline-offset-2" href="https://rocketseat.com.br">Rocketseat</a>
